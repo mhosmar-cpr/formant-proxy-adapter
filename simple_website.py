9c83a08c1547af30a87a7e4f581dc57feafbd127
mike hosmar <mikehosmar@gmail.com>
2022-05-31 16:12:35 +0000

    make it possible to launch simple_website with python

--- simple_website.py
+++ simple_website.py
@@ -11,3 +11,10 @@ def hello_world_get():
 @app.route("/test", methods=["POST"])
 def hello_world_post():
     return jsonify({"return_message": "goodye world"})
+    
+# main driver function
+if __name__ == '__main__':
+  
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
